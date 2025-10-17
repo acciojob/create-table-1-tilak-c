4874e83ba94d3c0951bdd059a46fcc260fc6b8cd
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 16:16:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
+  const tableBody=document.getElementById("sampleTable");
+	// let rows=parseInt(document.querySelectorAll('tr'));
+	// let length=rows.length;
+	let row=document.createElement('tr');
+	row.innerHTML=`
+	<td>New Cell1</td>
+	<td>New Cell2</td>
+	`
+	tableBody.insertBefore(row,tableBody.firstElementChild);
+
   
 }
