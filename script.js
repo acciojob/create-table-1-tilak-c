ff16b95e43f788d359ca48121b51d865b189f160
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 16:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 function insert_Row() {
     //Write your code here
-  const tableBody=document.getElementById("sampleTable");
-	// let rows=parseInt(document.querySelectorAll('tr'));
-	// let length=rows.length;
-	let row=document.createElement('tr');
-	row.innerHTML=`
-	<td>New Cell1</td>
-	<td>New Cell2</td>
-	`
-	// tableBody.insertBefore(row,tableBody.firstElementChild);
-	tableBody.appendChild(row);
+  const table=document.getElementById("sampleTable");
+	let row=table.insertRow(0);
+	let cell1=row.insertCell(0);
+	let cell2=row.insertCell(1);
+	cell1.innerHTML="New Cell1";
+	cell2.innerHTML="New Cell2";
+	tableBody.insertBefore(row,tableBody.firstElementChild);
+	// tableBody.appendChild(row);
   
 }
